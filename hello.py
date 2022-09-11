ca8ae8fa6435cb50850a8fdab52c85d8b7dd0425
Allison Kroboth <42.allison.k@gmail.com>
2022-09-11 15:14:47 -0400

    trying things

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@
 def hello_world():
     # WRITE ALL OF YOUR CODE (INDENTED) HERE
     # print out the string 'Hello World!'
-    print("Hello World")
+    print("hello world")
 
 hello_world()
     
